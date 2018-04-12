353d3a3fe3746a0d7b312bdfb28720c8e7d77ac7
zhouy546 <zhouyunqi546@gmail.com>
2018-04-12 12:14:32 +0800

    bugfix

--- Assets/EasyAR/Scripts/CameraDeviceBehaviour.cs
+++ Assets/EasyAR/Scripts/CameraDeviceBehaviour.cs
@@ -19,6 +19,8 @@ using EasyAR;
         public Color ScreenRunningColor;
         [SerializeField]
         RawImage rawImage;
+        [SerializeField]
+        Text text;
         public int ScreenProtectWaitTime;
         private int CountDownTime;
         private bool IsWakeUp=true;
@@ -71,7 +73,8 @@ using EasyAR;
         void Sleep()
         {
             IsWakeUp = false;
-        ChangeAlpha(rawImage, 0, 1, 1f);
+        ChangeRawImageAlpha(rawImage, 0, 1, 1f);
+        ChangeTextAlpha(text, 0, 1, 1f);
 
         StopCapture();
             Close();
@@ -80,15 +83,22 @@ using EasyAR;
         void WakeUp()
         {
             IsWakeUp = true;
-        ChangeAlpha(rawImage, 1, 0, 1f);
+        ChangeRawImageAlpha(rawImage, 1, 0, 1f);
+        ChangeTextAlpha(text, 1, 0, 1f);
             StartCapture();
             OpenAndStart();
         }
 
 
-        void ChangeAlpha(RawImage rawImage,float from, float to, float time) {
+        void ChangeRawImageAlpha(RawImage rawImage,float from, float to, float time) {
             LeanTween.value(from, to, time).setOnUpdate((float value)=>{
                 rawImage.color = new Color(rawImage.color.r, rawImage.color.g, rawImage.color.b, value);
         });
+        }
+
+        void ChangeTextAlpha(Text text, float from, float to, float time) {
+            LeanTween.value(from, to, time).setOnUpdate((float value) => {
+                text.color = new Color(text.color.r, text.color.g, text.color.b, value);
+            });
         }
     }
